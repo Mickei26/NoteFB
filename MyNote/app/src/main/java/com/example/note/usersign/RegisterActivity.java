0ae0e2f26f0aa83b38a56c62be03210f0c73a337
Dam Nguyen <dammit2525@yahoo.com.vn>
2022-05-13 15:40:09 +0700

    update

--- MyNote/app/src/main/java/com/example/note/usersign/RegisterActivity.java
+++ MyNote/app/src/main/java/com/example/note/usersign/RegisterActivity.java
@@ -14,6 +14,8 @@ import com.example.note.MainActivity;
 import com.example.note.R;
 import com.example.note.StartActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,7 +25,6 @@ import com.google.firebase.database.FirebaseDatabase;
 public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth fAuth;
-//    private DatabaseReference fUsersDatabase;
     private FirebaseDatabase database;
     private DatabaseReference myRef;
 
@@ -42,7 +43,6 @@ public class RegisterActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         // below line is used to get reference for our database.
         myRef = database.getReference().child("Users");
-//        fUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
 
         btnRegister = findViewById(R.id.btnRegister);
         btnBack = findViewById(R.id.btnBack);
@@ -60,6 +60,7 @@ public class RegisterActivity extends AppCompatActivity {
                 registerUser(name, email, pass);
             }
         });
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,12 +78,13 @@ public class RegisterActivity extends AppCompatActivity {
                 if(task.isSuccessful()){
 //                    Intent startIntent = new Intent(RegisterActivity.this, StartActivity.class);
 //                    startActivity(startIntent);
-//                    finish();
 //                    Toast.makeText(RegisterActivity.this, "User created!", Toast.LENGTH_SHORT).show();
+//                    String userId = myRef.push().getKey();
+//                    myRef.child("UserId").setValue(name);
 //                    myRef.child(fAuth.getCurrentUser().getUid()).child("basic").child("name").setValue(name);
 //                    fAuth.signOut();
 //                    hàm lưu name user vào trong Realtime Database
-                    myRef.child(fAuth.getCurrentUser().getUid()).child("basic").child("name").push().setValue(name).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    myRef.child(fAuth.getCurrentUser().getUid()).child("basic").child("name").setValue(name).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()){
