634c6283a93c17c601aab4b79a41f96ef086e180
Dam Nguyen <dammit2525@yahoo.com.vn>
2022-05-17 17:15:59 +0700

    update

--- MyNote/app/src/main/java/com/example/note/MainActivity.java
+++ MyNote/app/src/main/java/com/example/note/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.note;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.note.Note.NewNoteActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -45,5 +49,28 @@ public class MainActivity extends AppCompatActivity {
             finish();
             Log.i("MainActivity", "fAuth == null");
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()){
+            case R.id.add_note:
+                Intent startIntent = new Intent(MainActivity.this, NewNoteActivity.class);
+                startActivity(startIntent);
+                break;
+        }
+
+        return true;
     }
 }
