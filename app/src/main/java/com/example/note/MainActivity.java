df395f6466b6f958556ac7540ef1ab98992fdd5e
Dam Nguyen <dammit2525@yahoo.com.vn>
2022-05-28 11:00:07 +0700

    update

--- app/src/main/java/com/example/note/MainActivity.java
+++ app/src/main/java/com/example/note/MainActivity.java
@@ -20,6 +20,7 @@ import android.widget.Toast;
 import com.example.note.more.ListNoteAdapter;
 import com.example.note.notes.NewNoteActivity;
 import com.example.note.notes.Note;
+import com.example.note.notes.ShowNoteActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -86,10 +87,10 @@ public class MainActivity extends AppCompatActivity {
 
                             String test = list.get(position).getTitle();
                             Toast.makeText(getApplicationContext(), test, Toast.LENGTH_LONG).show();
-                            EditText editTitle = (EditText)findViewById(R.id.editTitle);
+                            EditText editTitle = (EditText)findViewById(R.id.showTitle);
                             editTitle.setText(test);
 
-                            Intent intent = new Intent(MainActivity.this, NewNoteActivity.class);
+                            Intent intent = new Intent(MainActivity.this, ShowNoteActivity.class);
 //                            intent.putExtra("location", editText.getText(test));
 //                            intent.putExtra("p",position);
                             startActivity(intent);
---
+++ app/src/main/java/com/example/note/notes/ShowNoteActivity.java
@@ -0,0 +1,16 @@
+package com.example.note.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.note.R;
+
+public class ShowNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_note);
+    }
+}
