688e4f1aeee12922071e7e69e2a73fd31b5e4033
Dam Nguyen <dammit2525@yahoo.com.vn>
2022-05-28 12:25:17 +0700

    Update ShowNoteActivity.java

--- app/src/main/java/com/example/note/notes/ShowNoteActivity.java
+++ app/src/main/java/com/example/note/notes/ShowNoteActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,8 +17,11 @@ import com.example.note.MainActivity;
 import com.example.note.R;
 import com.example.note.StartActivity;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ShowNoteActivity extends AppCompatActivity {
 
@@ -34,6 +38,7 @@ public class ShowNoteActivity extends AppCompatActivity {
 
         fAuth = FirebaseAuth.getInstance();
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://note-2606-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        myRef = database.getReference( "Notes");
 
         showTitle = findViewById(R.id.showTitle);
         String title = getIntent().getExtras().getString("Title");
@@ -59,6 +64,20 @@ public class ShowNoteActivity extends AppCompatActivity {
         });
     }
 
+    public void editNote(){
+        myRef.child(fAuth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("Failed to edit value.", error.toException());
+            }
+        });
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
@@ -73,6 +92,7 @@ public class ShowNoteActivity extends AppCompatActivity {
 
         switch (item.getItemId()){
             case R.id.edit_note:
+                editNote();
                 Intent startIntent = new Intent(ShowNoteActivity.this, NewNoteActivity.class);
                 startActivity(startIntent);
                 break;
